a387ff57a9f874cf36e4ff169578235735dc7aa4
mehdi abbad <doomedfox@gmail.com>
2018-10-28 21:42:38 +0100

    First version

---
+++ precache-manifest.df5fefb96b7c6dbe161f942e9db56e59.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8baed569752602d58369c57b5e6fa46",
+    "url": "/static/media/erlang.c8baed56.png"
+  },
+  {
+    "revision": "89c7966d7459fec3a8de",
+    "url": "/static/css/main.57dc468e.chunk.css"
+  },
+  {
+    "revision": "7a82dfdd61b62b3ca5adb0b44d635a79",
+    "url": "/static/media/qgis_layer_slicer.7a82dfdd.gif"
+  },
+  {
+    "revision": "4fb8f665a7146c1a3032",
+    "url": "/static/js/1.4fb8f665.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03e291473191b5b2340ffab62b4b919e",
+    "url": "/static/media/me.03e29147.png"
+  },
+  {
+    "revision": "682c52241586d7f468c6ead9b56e6659",
+    "url": "/static/media/bg2.682c5224.jpg"
+  },
+  {
+    "revision": "0f6558f88fb457e68f92c042df252892",
+    "url": "/static/media/react.0f6558f8.png"
+  },
+  {
+    "revision": "d08330ea31ccc7111c9d9fc9d80376ff",
+    "url": "/static/media/nginx.d08330ea.png"
+  },
+  {
+    "revision": "e8b602700ebdac7dd369834a3275bc46",
+    "url": "/static/media/GraphQL.e8b60270.png"
+  },
+  {
+    "revision": "fa631eee113da39872884438dcbbbe07",
+    "url": "/static/media/PgSQL.fa631eee.png"
+  },
+  {
+    "revision": "1a6e71a8b75291f8a3d7b7e6d58f777e",
+    "url": "/static/media/svn.1a6e71a8.png"
+  },
+  {
+    "revision": "338f19dcd02e1d13cc6ae6b279f65de5",
+    "url": "/static/media/python.338f19dc.png"
+  },
+  {
+    "revision": "89c7966d7459fec3a8de",
+    "url": "/static/js/main.89c7966d.chunk.js"
+  },
+  {
+    "revision": "8f42e018a9340b6e4e20161b06525308",
+    "url": "/static/media/C++.8f42e018.png"
+  },
+  {
+    "revision": "648bdba6b54bddd0297100a2a9b097a5",
+    "url": "/static/media/C.648bdba6.png"
+  },
+  {
+    "revision": "58ff4c7d62bf2e076ad422f6f4f3bae0",
+    "url": "/static/media/Qt5.58ff4c7d.png"
+  },
+  {
+    "revision": "33b5e3716cf072726787084706292d60",
+    "url": "/static/media/titanium_sdk.33b5e371.png"
+  },
+  {
+    "revision": "f86ae244b787a95152a788ab3c980d5e",
+    "url": "/static/media/Android.f86ae244.png"
+  },
+  {
+    "revision": "db612a1a8f27789dbd12a9c05da5a5b9",
+    "url": "/static/media/CAMA_screen.db612a1a.png"
+  },
+  {
+    "revision": "5b774ccaf9ec840ee3bf02441d8fa94e",
+    "url": "/static/media/AMD.5b774cca.gif"
+  },
+  {
+    "revision": "0859c95b6482e0c3bc12a27a4aa4de7a",
+    "url": "/static/media/KEN.0859c95b.png"
+  },
+  {
+    "revision": "a56b7391a740bf58d28129da89df9eae",
+    "url": "/static/media/ITS_logo.a56b7391.png"
+  },
+  {
+    "revision": "d443c0b30673da1fff6d1327c5809584",
+    "url": "/static/media/loquium.d443c0b3.png"
+  },
+  {
+    "revision": "72eff463e3e64abf0970ee0b26deadaf",
+    "url": "/static/media/OPA_v1.72eff463.png"
+  },
+  {
+    "revision": "4fb8f665a7146c1a3032",
+    "url": "/static/css/1.66eb7191.chunk.css"
+  },
+  {
+    "revision": "2f785376dadb224978a61ebd1e1d6213",
+    "url": "/index.html"
+  }
+];
